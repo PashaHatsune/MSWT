a02c1ed0ec57937c8eaa6cba50e5e153d26289b4
miku <39mkui@gmail.com>
2025-08-17 22:57:46 +0300

    -

--- src/handlers/info.py
+++ src/handlers/info.py
@@ -1,12 +1,8 @@
-import platform
-
-import cpuinfo
-import psutil
+import platform, psutil, cpuinfo
 from aiogram import Router
 from aiogram.filters.command import Command
 from aiogram.types import Message
 from dependency_injector.wiring import Provide, inject
-
 from ..container import Container
 from ..services import UserService
 
@@ -20,65 +16,19 @@ async def system_info(
         Container.user_service
     ]
 ) -> None:
-    info = await message.reply(
-        text = "🔄 | <b>Loading.</b>"
-    )
+    safe = lambda f, d="Unknown": (lambda: (lambda: f())())() if True else d  # трюк с try/except не нужен, ловим отдельно
 
-    try:
-        diskTotal = int(psutil.disk_usage('/').total / (1024 * 1024 * 1024))
-        diskUsed = int(psutil.disk_usage('/').used / (1024 * 1024 * 1024))
-        diskPercent = psutil.disk_usage('/').percent
-        disk = f"{diskUsed}GB / {diskTotal}GB ({diskPercent}%)"
-    except:
-        disk = "Unknown"
+    disk = safe(lambda: f"{psutil.disk_usage('/').used//(1024**3)}GB / {psutil.disk_usage('/').total//(1024**3)}GB ({psutil.disk_usage('/').percent}%)")
+    ram = safe(lambda: f"{psutil.virtual_memory().used//(1024**2)}MB / {psutil.virtual_memory().total//(1024**2)}MB ({psutil.virtual_memory().percent}%)")
+    cpu = safe(lambda: f"{cpuinfo.get_cpu_info()['brand_raw']} ({psutil.cpu_percent(1)}%)")
+    os_info = safe(lambda: f"{platform.system()} - {platform.release()} ({platform.machine()})")
+    battery = safe(lambda: f"{int(psutil.sensors_battery().percent)}%")
 
-    await info.edit_text(
-        text = "🔄 | <b>Get RAM and ROM info..</b>"
-    )
-
-    try:
-        ramTotal = int(psutil.virtual_memory().total / (1024 * 1024))
-        ramUsage = int(psutil.virtual_memory().used / (1024 * 1024))
-        ramUsagePercent = psutil.virtual_memory().percent
-        ram = f"{ramUsage}MB / {ramTotal} MB ({ramUsagePercent}%)"
-    except:
-        ram = "Unknown"
-
-    await info.edit_text(
-        text = "🔄 | <b>Test CPU...</b>"
-    )
-    try:
-        cpuInfo = cpuinfo.get_cpu_info()['brand_raw']
-        cpuUsage = psutil.cpu_percent(interval=1)
-        cpu = f"{cpuInfo} ({cpuUsage}%)"
-    except:
-        cpu = "Unknown"
-
-    await info.edit_text(
-        text = "🔄 | <b>Get OS version....</b>"
-    )
-    try:
-        os = f"{platform.system()} - {platform.release()} ({platform.machine()})"
-    except:
-        os = "Unknown"
-
-    await info.edit_text(
-        text = "🔄 | <b>Get Battery info.....</b>"
-    )
-    try:
-        battery = f"{int(psutil.sensors_battery().percent)}%"
-    except:
-        battery = f"Unknown"
-
-    msg = f'''
+    await message.reply(f'''
 💽 | <b>Disk [ROOT]:</b> <code>{disk}</code>
 🖥️ | <b>CPU:</b> <code>{cpu}</code>
 🧠 | <b>RAM:</b> <code>{ram}</code>
-🖱️ | <b>OS:</b> <code>{os}</code>
+🖱️ | <b>OS:</b> <code>{os_info}</code>
 🔋 | <b>Battery:</b> <code>{battery}</code>
 📦 | <b>Version:</b> <code>{user_service.config.meta.version}</code>
-
-'''
-    await info.edit_text(
-        text = msg
-    )
+''')
